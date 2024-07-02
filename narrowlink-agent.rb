cc085f434657dbeafa3519b21ed73bcc87acacce
SajjadPourali <sajjad@pourali.com>
2024-07-02 14:29:32 -0400

    Fix brew audit

--- narrowlink-agent.rb
+++ narrowlink-agent.rb
@@ -1,5 +1,5 @@
 class NarrowlinkAgent < Formula
-  desc "A self-hosted solution to enable secure connectivity between devices across restricted networks"
+  desc "Self-hosted solution for secure device connectivity across restricted networks"
   homepage "https://github.com/narrowlink/narrowlink"
   url "https://github.com/narrowlink/narrowlink/archive/refs/tags/0.2.6.tar.gz"
   sha256 "6682e742431fa3496cbfd550039c6fe46c87fbce6a74d0d2c227cae38e8e2dd3"
@@ -23,16 +23,11 @@ class NarrowlinkAgent < Formula
 
   depends_on "rust" => :build
 
-  # on_linux do
-  #   depends_on "pkg-config" => :build
-  #   depends_on "openssl@3"
-  # end
-
   def install
     system "cargo", "install", *std_cargo_args(path: "agent")
   end
 
-  # test do
-  #   system "#{bin}/narrowlink", "-t", "literal:nl", "assert:nl"
-  # end
+  test do
+    assert_match "Narrowlink Agent", shell_output("#{bin}/narrowlink-agent --version")
+  end
 end
--- narrowlink.rb
+++ narrowlink.rb
@@ -1,5 +1,5 @@
 class Narrowlink < Formula
-  desc "A self-hosted solution to enable secure connectivity between devices across restricted networks"
+  desc "Self-hosted solution for secure device connectivity across restricted networks"
   homepage "https://github.com/narrowlink/narrowlink"
   url "https://github.com/narrowlink/narrowlink/archive/refs/tags/0.2.6.tar.gz"
   sha256 "6682e742431fa3496cbfd550039c6fe46c87fbce6a74d0d2c227cae38e8e2dd3"
@@ -23,11 +23,6 @@ class Narrowlink < Formula
 
   depends_on "rust" => :build
 
-  # on_linux do
-  #   depends_on "pkg-config" => :build
-  #   depends_on "openssl@3"
-  # end
-
   def install
     system "cargo", "install", *std_cargo_args(path: "client")
     system "cargo", "install", *std_cargo_args(path: "agent")
@@ -35,7 +30,10 @@ class Narrowlink < Formula
     system "cargo", "install", *std_cargo_args(path: "token-generator")
   end
 
-  # test do
-  #   system "#{bin}/narrowlink", "-t", "literal:nl", "assert:nl"
-  # end
+  test do
+    assert_match "Narrowlink Client", shell_output("#{bin}/narrowlink --version")
+    assert_match "Narrowlink Agent", shell_output("#{bin}/narrowlink-agent --version")
+    assert_match "Narrowlink Gateway", shell_output("#{bin}/narrowlink-gateway --version")
+    assert_match "Narrowlink Token Generator", shell_output("#{bin}/narrowlink-token-generator --version")
+  end
 end
